672b9baf988741cc564322284d446ea691762671
Michael Feher <michael.feher@algorand.foundation>
2024-12-23 20:09:16 -0500

    chore: fix stop fast catchup verbiage

--- cmd/node/catchup/stop.go
+++ cmd/node/catchup/stop.go
@@ -6,16 +6,30 @@ import (
 	"github.com/algorandfoundation/algorun-tui/cmd/utils"
 	"github.com/algorandfoundation/algorun-tui/internal/algod"
 	"github.com/algorandfoundation/algorun-tui/ui/style"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+var stopCmdShort = "Stop a fast catchup"
+var stopCmdLong = lipgloss.JoinVertical(
+	lipgloss.Left,
+	style.Purple(style.BANNER),
+	"",
+	style.Bold(stopCmdShort),
+	"",
+	style.BoldUnderline("Overview:"),
+	"Stop an active Fast-Catchup. This will abort the catchup process if one has started",
+	"",
+	style.Yellow.Render("Note: Not all networks support Fast-Catchup."),
+)
+
 // stopCmd is a Cobra command used to check the node's sync status and initiate a fast catchup when necessary.
 var stopCmd = utils.WithAlgodFlags(&cobra.Command{
 	Use:          "stop",
-	Short:        "Start a fast catchup",
-	Long:         "Checks if the node is caught up and if not, starts catching up.",
+	Short:        stopCmdShort,
+	Long:         stopCmdLong,
 	SilenceUsage: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Load configuration
