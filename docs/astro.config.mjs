e8e28c55e047f32ad960bef52f8e489ec2d425e3
Tasos Bitsios <tasosbitsios@algorandfndation>
2025-01-07 14:35:39 +0100

    chore: docs: disable guides for now

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -16,16 +16,16 @@ export default defineConfig({
 				github: 'https://github.com/algorandfoundation/nodekit',
 			},
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
 				{
 					label: 'Reference',
-					collapsed: true,
+					// collapsed: true,
 					autogenerate: { directory: 'reference' },
 				},
 			],
